66be2de05305920a89a265ce52561c77d2783bd6
ro-double-b <robb.marsee@gmail.com>
2016-06-22 20:55:13 -0700

    Passed all test cases for SongQueue.

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,13 +4,36 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
-    
+    // Add song listener
+    this.on('add', function() {
+      if (this.models.length === 1) {
+        // If song is the only song in queue, call playFirst()
+        this.playFirst();
+      }
+    });
+
+    // Once the song has ended
+    this.on('ended', function() {
+      // Dequeue the song
+      this.models[0].dequeue();
+      
+      if (this.models.length > 0) {
+        // If there are any songs left, call playFirst()
+        this.playFirst();
+      }
+    }, this);
+
+    // When dequeue on song is called
+    this.on('dequeue', function() {
+      // remove song from queue
+      this.models[0].destroy();
+    });
+
   },
 
+  // plays first song on the queue
   playFirst: function() {
-    this.on('enqueue', function() {
-
-    });
+    this.models[0].play();
   }
 
 });
--- client/models/SongModel.js
+++ client/models/SongModel.js
@@ -15,7 +15,6 @@ var SongModel = Backbone.Model.extend({
 
   ended: function() {
     this.trigger('ended', this);
-  },
-
+  }
 
 });
